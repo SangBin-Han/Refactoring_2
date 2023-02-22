1c3bde6585f5324e194523daac7aeba5b1f41755
SangBin-Han <sk8er092@gmail.com>
2023-02-22 19:25:03 +0900

    [MOD] 10.3 조건문의 not 연산자 제거

--- Ch10/3_replaceNestedConditionalWithGuardClauses.js
+++ Ch10/3_replaceNestedConditionalWithGuardClauses.js
@@ -81,7 +81,7 @@ function adjustedCapital_ex(anInstrument) {
 function adjustedCapital(anInstrument) {
   let result = 0;
   if (anInstrument.capital <= 0) return result;
-  if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) return result;
+  if ((anInstrument.interestRate <= 0 || anInstrument.duration <= 0)) return result;
   result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
   return result;
 }
