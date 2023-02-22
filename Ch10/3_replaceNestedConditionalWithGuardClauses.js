1ede1ccc5da810def782b6b6e17387888655818b
SangBin-Han <sk8er092@gmail.com>
2023-02-22 19:18:37 +0900

    [MOD] 10.3 중첩 조건문을 보호 구문으로 바꾸기

--- Ch10/3_replaceNestedConditionalWithGuardClauses.js
+++ Ch10/3_replaceNestedConditionalWithGuardClauses.js
@@ -30,7 +30,7 @@ class DefaultExample {
 
 // --------------- 예시1 ------------------
 
-function payAmount(employee) {
+function payAmount_ex(employee) {
   let result;
   if (employee.isSeparated) { // 퇴사한 직원인가?
     result = {amount: 0, reasonCode: "SEP"};
@@ -47,6 +47,25 @@ function payAmount(employee) {
       ut.enim.ad(minim.veniam);
       result = someFinalComputation();
     }
+  }
+  return result;
+}
+
+// --------------- Refactoring ------------------
+
+function payAmount(employee) {
+  let result;
+  if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+  if (employee.isRetired) { // 은퇴한 직원인가?
+    result = {amount: 0, reasonCode: "RET"};
+  }
+  else {
+    // 급여 계산 로직
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(minim.veniam);
+    result = someFinalComputation();
   }
   return result;
 }
