8bb908fb9a82fca7b792fb4955fd594fda388f9d
SangBin-Han <sk8er092@gmail.com>
2023-02-22 18:56:01 +0900

    [MOD] 10.2 조건문 2개 통합

--- Ch10/2_consolidate_conditional_expression/consolidateConditionalExpression.js
+++ Ch10/2_consolidate_conditional_expression/consolidateConditionalExpression.js
@@ -36,8 +36,8 @@ function disabilityAmount_ex(anEmployee) {
 // --------------- Refactoring ------------------
 
 function disabilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthsDisabled > 12) return 0;
+  if ((anEmployee.seniority < 2)
+    || (anEmployee.monthsDisabled > 12)) return 0;
   if (anEmployee.isPartTime) return 0;
   // 장애 수단 계산
 }
