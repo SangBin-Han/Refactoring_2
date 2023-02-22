0fe9945a2d4e937d524dff1c143d6345b8e089a9
SangBin-Han <sk8er092@gmail.com>
2023-02-22 18:42:39 +0900

    [MOD] 10.1 삼항연산자로 변환

--- Ch10/1_decompose_conditional/decomposeConditional.js
+++ Ch10/1_decompose_conditional/decomposeConditional.js
@@ -18,7 +18,7 @@
  */
 
 function ex() {
-  if (!acquireData_ex.isBefore(plan.summerStart) && !acquireData_ex.isAfter(plan.summerEnd))
+  if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
     charge = quantity * plan.summerRate;
   else
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
@@ -27,14 +27,11 @@ function ex() {
 // --------------- Refactoring ------------------
 
 function refactoring() {
-  if (summer())
-    charge = summerCharge();
-  else
-    charge = regularCharge();
+  charge = summer() ? summerCharge() : regularCharge();
 }
 
 function summer() {
-  return !acquireData_ex.isBefore(plan.summerStart) && !acquireData_ex.isAfter(plan.summerEnd);
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
 }
 function summerCharge() {
   return quantity * plan.summerRate;
