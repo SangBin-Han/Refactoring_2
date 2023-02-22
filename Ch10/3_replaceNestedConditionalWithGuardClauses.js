bcbf492abbd5569746151ad39b57a60b6ae88cf0
SangBin-Han <sk8er092@gmail.com>
2023-02-22 19:23:24 +0900

    [MOD] 10.3 최상위 조건 보호 구문으로 수정

--- Ch10/3_replaceNestedConditionalWithGuardClauses.js
+++ Ch10/3_replaceNestedConditionalWithGuardClauses.js
@@ -66,12 +66,23 @@ function payAmount(employee) {
 
 // --------------- 예시2: 조건 반대로 만들기 ------------------
 
-function adjustedCapital(anInstrument) {
+function adjustedCapital_ex(anInstrument) {
   let result = 0;
   if (anInstrument.capital > 0) {
     if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
       result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
+  }
+  return result;
+}
+
+// --------------- Refactoring ------------------
+
+function adjustedCapital(anInstrument) {
+  let result = 0;
+  if (anInstrument.capital <= 0) return result;
+  if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
   }
   return result;
 }
