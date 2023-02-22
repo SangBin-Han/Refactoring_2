8778075b3c47896726ac9b04b45195f2841dc157
SangBin-Han <sk8er092@gmail.com>
2023-02-22 18:59:35 +0900

    [MOD] and 사용 예시 추가

--- Ch10/2_consolidate_conditional_expression/consolidateConditionalExpression.js
+++ Ch10/2_consolidate_conditional_expression/consolidateConditionalExpression.js
@@ -26,6 +26,8 @@ function ex1_refactoring() {
   }
 }
 
+// --------------- 예시: or 사용하기 ------------------
+
 function disabilityAmount_ex(anEmployee) {
   if (anEmployee.seniority < 2) return 0;
   if (anEmployee.monthsDisabled > 12) return 0;
@@ -38,10 +40,28 @@ function disabilityAmount_ex(anEmployee) {
 function disabilityAmount(anEmployee) {
   if (isNotEligibleForDisability()) return 0;
   // 장애 수단 계산
-  
+
   function isNotEligibleForDisability() {
     return ((anEmployee.seniority < 2)
       || (anEmployee.monthsDisabled > 12)
       || (anEmployee.isPartTime));
   }
+}
+
+// --------------- 예시: and 사용하기 ------------------
+
+function and_ex() {
+  if (anEmployee.onVacation)
+    if (anEmployee.seniority > 10)
+      return 1;
+  return 0.5;
+}
+
+// --------------- Refactoring ------------------
+
+function and_ex() {
+  if (anEmployee.onVacation)
+    if (anEmployee.seniority > 10)
+      return 1;
+  return 0.5;
 }
