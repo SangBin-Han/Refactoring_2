b90cccf599622909876b70ab223bd845286ee8a5
SangBin-Han <sk8er092@gmail.com>
2023-02-24 20:52:33 +0900

    [MOD] 10.7 제어 플래그 제거

--- Ch10/7_replaceControlFlagWithBreak.js
+++ Ch10/7_replaceControlFlagWithBreak.js
@@ -31,17 +31,14 @@ function ex() {
   checkForMiscreants(people);
 }
 function checkForMiscreants(people) {
-  let found = false;
   for (const p of people) {
-    if (!found) {
-      if (p === "조커") {
-        sendAlert();
-        return;
-      }
-      if (p === "사루만") {
-        sendAlert();
-        found = true;
-      }
+    if (p === "조커") {
+      sendAlert();
+      return;
+    }
+    if (p === "사루만") {
+      sendAlert();
+      return;
     }
   }
 }
