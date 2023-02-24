af19dcc637c507fd46e492789e9d655ce1ce3d53
SangBin-Han <sk8er092@gmail.com>
2023-02-24 20:40:54 +0900

    [MOD] 10.6 끝

--- Ch10/6_introduceAssertion.js
+++ Ch10/6_introduceAssertion.js
@@ -16,6 +16,13 @@
 class Customer {
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
-    else return aNumber - (this.discountRate * aNumber);
+    else {
+      assert(this.discountRate >= 0);
+      return aNumber - (this.discountRate * aNumber);
+    }
+  }
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
   }
 }
