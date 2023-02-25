e23fdfae625263bbba88db1ee1bca0a54dc22c76
sbHan <sbhan@reconers.com>
2023-02-25 21:16:14 +0900

    [MOD] 11.1 더 가다듬기

--- Ch11/1_separateQueryFromModifier.js
+++ Ch11/1_separateQueryFromModifier.js
@@ -49,17 +49,7 @@ function main() {
   alertForMiscreant(people);
 }
 function alertForMiscreant(people) {
-  for (const p of people) {
-    if (p === "조커") {
-      setOffAlarms();
-      return;
-    }
-    if (p === "사루만") {
-      setOffAlarms();
-      return;
-    }
-  }
-  return "";
+  if (findMiscreant(people) !== "") setOffAlarms();
 }
 function findMiscreant(people) {
   for (const p of people) {
