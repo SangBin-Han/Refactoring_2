0b74bf59ff85ad4d52ff4287f197d984fcd2fad2
sbhan <sbhan@reconers.com>
2023-02-28 08:43:02 +0900

    [ADD] 11.3 예시2

--- Ch11/3_removeFlagArgument.js
+++ Ch11/3_removeFlagArgument.js
@@ -64,3 +64,31 @@ function regularDeliveryDate(anOrder) {
   else deliveryTime = 4;
   return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
+
+// --------------- 예시2: 매개변수를 까다로운 방식으로 사용할 때 ------------------
+
+function deliveryDate(anOrder, isRush) {
+  let result;
+  let deliveryTime;
+  if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT")
+    deliveryTime = isRush ? 1 : 2;
+  else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+    deliveryTime = 2;
+    if (anOrder.deliveryState === "NH" && isRush)
+      deliveryTime = 3;
+  }
+  else if (isRush)
+    deliveryTime = 3;
+  else if (anOrder.deliveryState === "ME")
+    deliveryTime = 3;
+  else
+    deliveryTime = 4;
+  result = anOrder.placedOn.plusDays(2 + deliveryTime);
+  if (isRush) result = result.minusDays(1);
+  return result;
+}
+
+// --------------- Refactoring ------------------
+
+function rushDeliveryDate(anOrder) {return deliveryDate(anOrder, true);}
+function regularDeliveryDate(anOrder) {return deliveryDate(anOrder, false);}
